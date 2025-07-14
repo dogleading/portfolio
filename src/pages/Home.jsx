f58a1a997a9ef8c232a3143ca496f1820d01a8ca
dawg <dogleading1@gmail.com>
2025-07-14 17:28:18 +0300

    added stars background animation, and dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
 export const Home = () => {
   return (
-    <div>
+    <main className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
+      <ThemeToggle />
 
       {/* Background Effect */}
+      <StarBackground />
 
       {/* Navbar */}
 
       {/* Main Content */}
 
       {/* Footer */}
-    </div>
+    </main>
   );
 };
